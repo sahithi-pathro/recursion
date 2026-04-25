f84ebb545037f311474d39cad172f033f36f5157
sahithi-pathro <sahithipathro@gmail.com>
2026-04-25 12:38:31 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void even(int i)
+{
+    if(i<-7)
+    {
+        return;
+    }
+    if(i%2==0)
+    {
+     printf("%d\n",i);   
+    }
+    even(i-1);
+}
+int main() {
+    even(4);
+}
