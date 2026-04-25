63dfa6dd3e4c92b3fc2f428555b24683c5f635eb
sahithi-pathro <sahithipathro@gmail.com>
2026-04-25 12:40:51 +0530

    Add files via upload

---
+++ sumdig.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int output(int a,int sum)
+{
+    if(a==0)
+    {
+        return sum;
+    }
+    int dig=a%10;
+    sum=sum+dig;
+    a=a/10;
+    output(a,sum);
+}
+int main() {
+    int a;
+    printf("Enter the  value:\n");
+    scanf("%d",&a);
+    int res=output(a,0);
+    printf("The sum is %d",res);
+    return 0;
+}
